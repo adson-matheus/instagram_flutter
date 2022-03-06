2d44fbf7c7cd2132dbe5c2eb766d88fcc040dd4a
Adson <adson.matheus62@gmail.com>
2022-03-06 12:00:04 -0300

    add layout dos stories e appbar com grid de fotos

--- lib/index/profile.dart
+++ lib/index/profile.dart
@@ -18,7 +18,7 @@ class Profile extends StatelessWidget {
               height: 100,
               child: ClipOval(
                 child: Image.asset(
-                  'assets/images/profile.jpg',
+                  'assets/images/adrya.jpg',
                   width: 327,
                   height: 327,
                 ),
@@ -34,9 +34,9 @@ class Profile extends StatelessWidget {
           ],
         ),
         Padding(
-          padding: const EdgeInsets.only(top: 12.0),
+          padding: const EdgeInsets.only(top: 12.0, bottom: 20.0),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.75,
@@ -48,21 +48,105 @@ class Profile extends StatelessWidget {
                   onPressed: () {},
                 ),
               ),
-              Row(
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: const Icon(
-                      Icons.person_add,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+              TextButton(
+                onPressed: () {},
+                child: const Icon(
+                  Icons.person_add,
+                  color: Colors.white,
+                ),
               ),
             ],
           ),
-        )
+        ),
+        const Text(
+          'Destaques dos stories',
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+        const Text('Mantenha seus stories favoritos no seu perfil',
+            style: TextStyle(
+              fontSize: 12,
+            )),
+        const ProfileStories(),
+        AppBarProfile(),
       ],
+    );
+  }
+}
+
+class ProfileStories extends StatelessWidget {
+  const ProfileStories({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: SizedBox(
+        height: 75,
+        width: 75,
+        child: ListView.builder(
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          itemBuilder: (BuildContext context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipOval(
+                child: Image.asset(
+                  'assets/images/adrya.jpg',
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class AppBarProfile extends StatelessWidget {
+  AppBarProfile({Key? key}) : super(key: key);
+
+  final List<Tab> _tab = const <Tab>[
+    Tab(
+      icon: Icon(Icons.grid_4x4),
+    ),
+    Tab(
+      icon: Icon(Icons.photo_camera_front_outlined),
+    ),
+  ];
+
+  final _tabBarView = <Widget>[
+    const Center(child: Text('Perfil')),
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        Icon(
+          Icons.photo_camera_front_outlined,
+          size: 40,
+        ),
+        Text('Fotos com você'),
+      ],
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.5,
+      child: DefaultTabController(
+        length: _tab.length,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: _tab,
+              indicatorColor: Colors.white,
+            ),
+            elevation: 0.0,
+            backgroundColor: Colors.grey[850],
+          ),
+          body: TabBarView(children: _tabBarView),
+        ),
+      ),
     );
   }
 }
