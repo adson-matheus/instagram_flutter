dfddd8a119d50755f19757626f0e39bf0c741f61
Adson <adson.matheus62@gmail.com>
2022-03-07 18:49:32 -0300

    add rotas para 1a pag e para config do perfil

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/account/new_user.dart';
 import 'package:instagram_flutter/index/main.dart';
+import 'package:instagram_flutter/index/profile/profile_settings.dart';
 import 'package:instagram_flutter/login/login.dart';
+import 'package:instagram_flutter/main.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
+    case '/':
+      return MaterialPageRoute(builder: (_) => const MainPage());
     case '/login':
       return MaterialPageRoute(builder: (_) => const LoginPage());
     case '/new/user':
@@ -13,6 +17,9 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       final user = settings.arguments as Map<String, dynamic>;
       return MaterialPageRoute(
           builder: (_) => BottomNavigationBarIndex(user: user));
+    case '/profile_settings':
+      final id = settings.arguments as int;
+      return MaterialPageRoute(builder: (_) => ProfileSettings(id: id));
     default:
       return MaterialPageRoute(builder: (_) => const LoginPage());
   }
