faf29c8bb45aac5728334896478e64f07d7c6138
adson62 <adson.matheus62@gmail.com>
2022-03-09 16:49:47 -0300

    widget do appbar com a grade de fotos postadas do usuario

---
+++ lib/index/profile/profile_appbar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class AppBarProfile extends StatelessWidget {
+  AppBarProfile({Key? key}) : super(key: key);
+
+  final List<Tab> _tab = const <Tab>[
+    Tab(
+      icon: Icon(Icons.grid_4x4),
+    ),
+    Tab(
+      icon: Icon(Icons.photo_camera_front_outlined),
+    ),
+  ];
+
+  final _tabBarView = <Widget>[
+    const Center(child: Text('Perfil')),
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        Icon(
+          Icons.photo_camera_front_outlined,
+          size: 40,
+        ),
+        Text('Fotos com você'),
+      ],
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.5,
+      child: DefaultTabController(
+        length: _tab.length,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: _tab,
+              indicatorColor: Colors.white,
+            ),
+            elevation: 0.0,
+            backgroundColor: Colors.grey[850],
+          ),
+          body: TabBarView(children: _tabBarView),
+        ),
+      ),
+    );
+  }
+}
