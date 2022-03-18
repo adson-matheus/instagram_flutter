461755fe35427f8ea992eec00efe45a388035b34
adson62 <adson.matheus62@gmail.com>
2022-03-18 16:06:09 -0300

    Widget de pesquisar usuários [sem usar searchfield]

--- lib/index/search/search.dart
+++ lib/index/search/search.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/models/user.dart';
-import 'package:searchfield/searchfield.dart';
 
 class SearchPageWidget extends StatefulWidget {
   const SearchPageWidget({Key? key}) : super(key: key);
@@ -10,76 +9,87 @@ class SearchPageWidget extends StatefulWidget {
 }
 
 class _SearchPageWidgetState extends State<SearchPageWidget> {
+  bool showList = false;
+  List<Map<String, dynamic>>? users;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<List<Map<String, dynamic>>?>(
-          future: getUsersAndProfilePictures(),
-          builder:
-              ((context, AsyncSnapshot<List<Map<String, dynamic>>?> snapshot) {
-            if (snapshot.hasData) {
-              return SingleChildScrollView(
-                child: SearchField(
-                  suggestions: snapshot.data!
-                      .map((user) => SearchFieldListItem(user['username'],
-                          item: user,
-                          child: ListTile(
-                            leading: SizedBox(
-                              width: 56,
-                              height: 100,
-                              child: ClipOval(
-                                child: Image.memory(
-                                  user['picture'],
-                                  width: 327,
-                                  height: 327,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            title: Text(
-                              user['username'],
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            subtitle: Text(
-                              user['name'],
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            contentPadding: EdgeInsets.zero,
-                          )))
-                      .toList(),
-                  hint: 'Pesquisar',
-                  hasOverlay: false,
-                  maxSuggestionsInViewPort: 5,
-                  itemHeight: 100.0,
-                  suggestionState: Suggestion.hidden,
-                  searchStyle: const TextStyle(
-                    height: 2.0,
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Pesquisar',
+            ),
+            keyboardType: TextInputType.name,
+            autocorrect: false,
+            autofocus: true,
+            maxLength: 50,
+            onSubmitted: (value) async {
+              if (value.isNotEmpty) {
+                final getUsers = await getUsersAndProfilePictures(value);
+                showList = true;
+                setState(() {
+                  users = getUsers;
+                });
+              }
+            },
+          ),
+        ),
+        if (showList) Expanded(child: ListUsersFromSearchWidget(users: users))
+      ],
+    );
+  }
+}
+
+class ListUsersFromSearchWidget extends StatelessWidget {
+  final List<Map<String, dynamic>>? users;
+  const ListUsersFromSearchWidget({Key? key, required this.users})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return users != null
+        ? ListView.builder(
+            itemCount: users!.length,
+            itemBuilder: ((context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: SizedBox(
+                    width: 56,
+                    height: 100,
+                    child: ClipOval(
+                      child: Image.memory(
+                        users![index]['picture'],
+                        width: 327,
+                        height: 327,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    users![index]['username'],
                     overflow: TextOverflow.ellipsis,
                   ),
-                  emptyWidget: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: const Align(
-                        alignment: Alignment.topCenter,
-                        child: Text('Sem resultados')),
+                  subtitle: Text(
+                    users![index]['name'],
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
                   ),
+                  contentPadding: EdgeInsets.zero,
+                  style: ListTileStyle.list,
+                  visualDensity: VisualDensity.adaptivePlatformDensity,
+                  horizontalTitleGap: 10,
+                  onTap: () {},
                 ),
               );
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            } else {
-              return Column(
-                children: const [
-                  Text('Carregando...'),
-                  Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: CircularProgressIndicator(),
-                  ),
-                ],
-              );
-            }
-          })),
-    );
+            }))
+        : const SizedBox(
+            child: Align(
+                alignment: Alignment.topCenter, child: Text('Sem resultados')),
+          );
   }
 }
