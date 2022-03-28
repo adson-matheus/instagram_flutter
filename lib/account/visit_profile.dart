da71a8edc24dafc85c7140df1380148d50bddf8c
adson62 <adson.matheus62@gmail.com>
2022-03-28 11:02:46 -0300

    perfil de um usuario, botao de seguir e visualizar followers

--- lib/account/visit_profile.dart
+++ lib/account/visit_profile.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_flutter/account/follow_an_account.dart';
 import 'package:instagram_flutter/controller/useful_widgets.dart';
 import 'package:instagram_flutter/index/profile/profile_appbar.dart';
 import 'package:instagram_flutter/index/profile/profile_stories.dart';
+import 'package:instagram_flutter/models/followers.dart';
 
 class VisitProfileWidget extends StatefulWidget {
   final Map<String, dynamic> user;
+  final int loggedUserId;
   const VisitProfileWidget({
     Key? key,
     required this.user,
+    required this.loggedUserId,
   }) : super(key: key);
 
   @override
@@ -17,11 +19,13 @@ class VisitProfileWidget extends StatefulWidget {
 
 class _VisitProfileWidgetState extends State<VisitProfileWidget> {
   late Map<String, dynamic> user;
+  late int loggedUserId;
 
   @override
   void initState() {
     super.initState();
     user = widget.user;
+    loggedUserId = widget.loggedUserId;
   }
 
   @override
@@ -56,8 +60,15 @@ class _VisitProfileWidgetState extends State<VisitProfileWidget> {
                 children: [
                   PaddingWithColumn(
                       number: user['totalPubs'], text: 'Publicações'),
-                  PaddingWithColumn(
-                      number: user['followers'], text: 'Seguidores'),
+                  TextButton(
+                    onPressed: () => Navigator.of(context)
+                        .pushNamed('/get_followers_from_user', arguments: [
+                      user['id'] as int,
+                      loggedUserId,
+                    ]),
+                    child: PaddingWithColumn(
+                        number: user['followers'], text: 'Seguidores'),
+                  ),
                   PaddingWithColumn(
                       number: user['following'], text: 'Seguindo'),
                 ],
@@ -80,7 +91,7 @@ class _VisitProfileWidgetState extends State<VisitProfileWidget> {
                           foregroundColor:
                               MaterialStateProperty.all(Colors.blue)),
                       onPressed: () async {
-                        final updated = await follow(user);
+                        final updated = await follow(user, loggedUserId);
                         setState(() {
                           user = updated;
                         });
