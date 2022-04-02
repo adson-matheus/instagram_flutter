f4a8dac21a28ce9048a26cff23697d7761b432fd
Adson <adson.matheus62@gmail.com>
2022-04-02 13:33:05 -0300

    simples botao para chamar a funcao postNewPicture

--- lib/index/post_picture/post_picture.dart
+++ lib/index/post_picture/post_picture.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/models/post_picture.dart';
+import 'package:instagram_flutter/models/user.dart';
 
 class PostPictureWidget extends StatefulWidget {
   final int loggedUserId;
@@ -12,8 +14,28 @@ class PostPictureWidget extends StatefulWidget {
 }
 
 class _PostPictureWidgetState extends State<PostPictureWidget> {
+  late int loggedUserId;
+  @override
+  void initState() {
+    super.initState();
+    loggedUserId = widget.loggedUserId;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextButton(
+      child: const Text('Postar Foto'),
+      onPressed: () async {
+        await postNewPicture(loggedUserId);
+
+        //posts count++
+        final user = await getUserById(loggedUserId);
+        // user['totalPubs']++;
+        // fromMap(user).update();
+
+        //back to index
+        Navigator.of(context).popAndPushNamed('/index', arguments: [user, 0]);
+      },
+    );
   }
 }
